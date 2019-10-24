6884e56faad8d56a10acdeb85fc394254d0cc699
yashviras <30821203+yashviras@users.noreply.github.com>
2019-10-24 23:05:16 +0530

    Update police.php

--- HireTruck-master/Admin/police.php
+++ HireTruck-master/Admin/police.php
@@ -4,40 +4,41 @@ require ('session.php');
 <!DOCTYPE html>
 <html lang="en">
   <head>
+  
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
     <title>NEW BID</title>
+    
     <link rel="icon" type="image/ico" href="https://i.ibb.co/GQ6gw34/1544624867669.png" />
+    
     <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" href="https://i.ibb.co/tDkQbmq/Logo.png" type="image/x-icon" />
         <title>Hire Truck</title>
-        <!-- Icon css link -->
         <link href="css/font-awesome.min.css" rel="stylesheet">
-        <!-- Bootstrap -->
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-        <!-- Rev slider css -->
         <link href="vendors/revolution/css/settings.css" rel="stylesheet">
         <link href="vendors/revolution/css/layers.css" rel="stylesheet">
         <link href="vendors/revolution/css/navigation.css" rel="stylesheet">
-        <!-- Extra plugin css -->
         <link href="vendors/owl-carousel/owl.carousel.min.css" rel="stylesheet">
         <link href="vendors/magnify-popup/magnific-popup.css" rel="stylesheet">
         <link href="css/style.css" rel="stylesheet">
         <link href="css/responsive.css" rel="stylesheet">
-<style media="screen">
+
+    <style media="screen">
 body{
  height: 100%;
  width:100%;
  }
 </style>
-    <!-- Bootstrap -->
+    
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"/>
     <link rel="stylesheet" type="text/css" href="css/logreg.css"/>
@@ -95,8 +96,6 @@ body{
                                         {
 
                     ?>
-
-
                                     <div class="row container">
                                         <div class="col">
                                            <?php print substr(md5($row['S_id']),-8); ?>
